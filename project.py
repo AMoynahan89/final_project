3646aeaa6bad85a6f81517ebf7038e9a506ee9f1
amoynahan89 <amfinsov@gmail.com>
2024-03-26 10:06:53 -0700

    Built scaffolding for project.py

--- project.py
+++ project.py
@@ -1,16 +1,21 @@
+import sqlite3
+
+
 def main():
-    get_user_id()
-    load_user_data()
-    save_user_data()
-    
+    get_or_create_user()
+    add_user_data()
+    recall_user_data()
 
-def get_user_id():
-
-
-def load_user_data():
+def get_or_create_user():
+    pass
 
 
-def save_user_data():
+def add_user_data():
+    pass
+
+
+def recall_user_data():
+    pass
 
 
 if __name__ == "__main__":
