dcad0981e83b9e670857406c9a3a7343570f14e8
amoynahan89 <amfinsov@gmail.com>
2024-03-29 00:59:31 -0700

    More dead code.

--- try_something.py
+++ try_something.py
@@ -20,7 +20,7 @@ print(result.fetchall())
 con.close()
 
 
-# Old code here
+# Old code below
 
 #def credentials_exist(username, db_manager):
 #        users = db_manager.execute_query("SELECT username FROM users")
@@ -29,3 +29,22 @@ con.close()
 #            return True
 #        else:
 #            return False
+
+
+#def get_or_create_user(username, password):
+#    with sqlite3.connect("database/my_database.db") as con:
+#        cursor = con.cursor()
+#        cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
+#        result = cursor.fetchone()
+#        if result:
+#            if result[0] == password:
+#                print(f"{username}, you are logged in!")
+                # Continue to the main program
+#            else:
+#                print("Incorrect password.")
+#        else:
+#            print(f"{username} does not exist. Creating account...")
+#            cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+#            con.commit()
+#            print("Account created. Please log in.")
+
